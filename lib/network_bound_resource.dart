2ebeec1abb28cb74ee742d67042138edefea33f3
dmitrijmihajluk <mikhajlyuk@gmail.com>
2024-01-08 20:10:29 +0100

    feat(general): update to Flutter 3

--- lib/network_bound_resource.dart
+++ lib/network_bound_resource.dart
@@ -1,11 +1,9 @@
 import 'dart:async';
 
 import 'package:api_error_parser/api_error_parser.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:network_utils/resource.dart';
 
-typedef SaveCallResult<ResultType, RequestType> = Future<ResultType> Function(
-    RequestType item);
+typedef SaveCallResult<ResultType, RequestType> = Future<ResultType> Function(RequestType item);
 
 typedef ShouldFetch<ResultType> = bool Function(ResultType data);
 
@@ -21,84 +19,82 @@ class NetworkBoundResource<ResultType, RequestType, T> {
   final ApiParser<T> _apiParser;
   final SaveCallResult<ResultType, RequestType> saveCallResult;
   final ShouldFetch<ResultType> shouldFetch;
-  final LoadFromCache<ResultType> loadFromCache;
+  final LoadFromCache<ResultType>? loadFromCache;
   final CreateCall createCall;
-  FetchFailed fetchFailed;
-  PaginationCall paginationCall;
+  FetchFailed? fetchFailed;
+  PaginationCall? paginationCall;
 
-  StreamController _resourceStream;
+  late StreamController _resourceStream;
 
-  NetworkBoundResource(this._apiParser,
-      {@required this.saveCallResult,
-      @required this.shouldFetch,
-      @required this.loadFromCache,
-      @required this.createCall,
-      this.fetchFailed,
-      this.paginationCall})
-      : assert(saveCallResult != null),
-        assert(shouldFetch != null),
-        assert(loadFromCache != null),
-        assert(createCall != null) {
-    if (fetchFailed == null) {
-      fetchFailed = () => {};
-    }
-    _resourceStream = StreamController<Resource<ResultType, T>>(onListen: () {
-      _startListenStream();
-    });
+  NetworkBoundResource(
+      this._apiParser, {
+        required this.createCall,
+        required this.shouldFetch,
+        required this.saveCallResult,
+        this.loadFromCache,
+        this.fetchFailed,
+        this.paginationCall,
+      }) {
+    fetchFailed ??= () {};
+    _resourceStream = StreamController<Resource<ResultType, T>>(onListen: _startListenStream);
   }
 
   void _startListenStream() {
     _resourceStream.add(Resource<ResultType, String>.loading(null));
-    var streamFromCache = this.loadFromCache();
 
-    if (streamFromCache != null) {
-      StreamSubscription scanSubscription;
-      scanSubscription = streamFromCache.listen((event) {
-        scanSubscription.cancel();
-        if (this.shouldFetch(event)) {
-          _fetchFromNetwork();
-        } else {
-          _resourceStream.add(Resource<ResultType, String>.success(event));
-        }
-      });
-    } else {
+    if (loadFromCache == null) {
       _fetchFromNetwork();
+      return;
     }
+
+    final streamFromCache = loadFromCache!();
+    late StreamSubscription scanSubscription;
+    scanSubscription = streamFromCache.listen((event) {
+      scanSubscription.cancel();
+      if (shouldFetch(event)) {
+        _fetchFromNetwork();
+      } else {
+        _resourceStream.add(Resource<ResultType, String>.success(event));
+      }
+    });
   }
 
-  void _fetchFromNetwork() async {
+  Future<void> _fetchFromNetwork() async {
     try {
-      final apiResponse = await createCall();
-      final ApiParserResponse<RequestType, T> parserResponse =
-          _apiParser.parse(apiResponse);
+      final dynamic apiResponse = await createCall();
+      final parserResponse = _apiParser.parse(apiResponse as ApiResponse<RequestType>);
       if (parserResponse is ApiParserSuccessResponse<RequestType, T>) {
-        if (parserResponse.pagination != null) {
-          paginationCall(parserResponse.pagination);
+        if (parserResponse.pagination != null && paginationCall != null) {
+          paginationCall!(parserResponse.pagination!);
         }
-        _resourceStream.add(Resource<ResultType, String>.success(
-            await saveCallResult(parserResponse.data),
-            pagination: parserResponse.pagination));
-      } else if (parserResponse is ApiParserEmptyResponse<RequestType, T>) {
         _resourceStream.add(
-            Resource<ResultType, String>.success(await saveCallResult(null)));
+          Resource<ResultType, String>.success(
+            await saveCallResult(parserResponse.data),
+            pagination: parserResponse.pagination,
+          ),
+        );
+      } else if (parserResponse is ApiParserEmptyResponse<RequestType, T>) {
+        _resourceStream.add(Resource<ResultType, String>.success(null));
       } else {
-        fetchFailed();
-        _resourceStream.add(Resource<ResultType, String>.errorList(
-            null, (parserResponse as ApiParserErrorResponse).errors));
+        fetchFailed!();
+        _resourceStream.add(
+          Resource<ResultType, String>.errorList(
+            null,
+            (parserResponse as ApiParserErrorResponse<RequestType, String>).errors,
+          ),
+        );
       }
-    } catch (e) {
-      fetchFailed();
+    } on Exception catch (e) {
+      fetchFailed!();
       if (e is Error) {
-        _resourceStream
-            .add(Resource<ResultType, String>.error(null, e.toString()));
+        _resourceStream.add(Resource<ResultType, String>.error(null, e.toString()));
       } else {
-        _resourceStream
-            .add(Resource<ResultType, String>.errorException(null, e));
+        _resourceStream.add(Resource<ResultType, String>.errorException(null, e));
       }
     }
   }
 
   Stream<Resource<ResultType, T>> asStream() {
-    return _resourceStream.stream;
+    return _resourceStream.stream as Stream<Resource<ResultType, T>>;
   }
 }
--- lib/resource.dart
+++ lib/resource.dart
@@ -1,39 +1,47 @@
 import 'package:api_error_parser/api_error_parser.dart';
 
-enum Status { success, error, loading }
+enum RequestStatus { success, error, loading }
 
 class Resource<T, E> {
-  final Status status;
-  final T data;
-  final Pagination pagination;
-  final Exception exception;
-  final String errorMessage;
-  final List<ParserMessageEntity<E>> errors;
+  final RequestStatus status;
+  final T? data;
+  final Pagination? pagination;
+  final Exception? exception;
+  final String? errorMessage;
+  final List<ParserMessageEntity<E>>? errors;
 
-  Resource._(this.status, this.data, this.pagination, this.exception, this.errorMessage, this.errors);
+  Resource._(
+      this.status,
+      this.data,
+      this.pagination,
+      this.exception,
+      this.errorMessage,
+      this.errors,
+      );
 
-  factory Resource.loading(T data) {
-    return Resource._(Status.loading, data, null, null, null, null);
+  factory Resource.loading(T? data) {
+    return Resource._(RequestStatus.loading, data, null, null, null, null);
   }
 
-  factory Resource.success(T data, {Pagination pagination}) {
-    return Resource._(Status.success, data, pagination, null, null, null);
+  factory Resource.success(T? data, {Pagination? pagination}) {
+    return Resource._(RequestStatus.success, data, pagination, null, null, null);
   }
 
-  factory Resource.error(T data, String errorMessage) {
-    return Resource._(Status.error, data, null, null, errorMessage, null);
+  factory Resource.error(T? data, String errorMessage) {
+    return Resource._(RequestStatus.error, data, null, null, errorMessage, null);
   }
 
-  factory Resource.errorException(T data, Exception exception) {
-    return Resource._(Status.error, data, null, exception, null, null);
+  factory Resource.errorException(T? data, Exception exception) {
+    return Resource._(RequestStatus.error, data, null, exception, null, null);
   }
 
-  factory Resource.errorList(T data, List<ParserMessageEntity<E>> errors) {
-    return Resource._(Status.error, data, null, null, null, errors);
+  factory Resource.errorList(T? data, List<ParserMessageEntity<E>> errors) {
+    return Resource._(RequestStatus.error, data, null, null, null, errors);
   }
 
   @override
   String toString() {
-    return "{ ${super.toString()} status = $status, data = $data, pagination = $pagination, exception = $exception, errorMessage = $errorMessage, errors = $errors}";
+    return '{ ${super.toString()} status = $status, data = $data, pagination = $pagination, exception = $exception,'
+        ' errorMessage = $errorMessage, errors = $errors}';
   }
 }
